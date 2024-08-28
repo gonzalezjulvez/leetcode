31bab7f744c291e855c259d2e61f29dd727e6458
Javier González <gonzalezjulvez@gmail.com>
2024-08-28 17:57:50 +0200

    Exercise 2

--- 002 - Add Two Numbers/Exercise.py
+++ 002 - Add Two Numbers/Exercise.py
@@ -1,5 +1,4 @@
-import unittest
-from typing import List
+from typing import Optional
 
 
 # Definition for singly-linked list.
@@ -10,36 +9,25 @@ class ListNode:
 
 
 class Solution:
-    def addTwoNumbers(self, l1: List[int], l2: List[int]) -> List[int]:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummy_head = ListNode(0)
+        current = dummy_head
         carry = 0
-        result = []
 
         while l1 or l2 or carry:
-            val1 = l1.pop(0) if l1 else 0
-            val2 = l2.pop(0) if l2 else 0
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
 
             total = val1 + val2 + carry
             carry = total // 10
-            result.append(total % 10)
-        return result
+            current.next = ListNode(total % 10)
+            current = current.next
 
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
 
-class TestSolution(unittest.TestCase):
-    def setUp(self):
-        self.s = Solution()
-
-    def test_case_1(self):
-        self.assertEqual(self.s.addTwoNumbers([2, 4, 3], [5, 6, 4]), [7, 0, 8])
-
-    def test_case_2(self):
-        self.assertEqual(self.s.addTwoNumbers([0], [0]), [0])
-
-    def test_case_3(self):
-        self.assertEqual(
-            self.s.addTwoNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]),
-            [8, 9, 9, 9, 0, 0, 0, 1],
-        )
-
-
-if __name__ == "__main__":
-    unittest.main()
+        return dummy_head.next
